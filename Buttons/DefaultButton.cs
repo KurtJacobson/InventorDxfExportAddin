58b27a18a9422302061c7274bc76baf3e5b6518a
Kurt Jacobson <kjacobson@metalforming-usa.net>
2025-09-30 14:22:30 -0400

    Initial commit

---
+++ Buttons/DefaultButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Forms;
+using Inventor;
+using Inventor.InternalNames.Ribbon;
+
+namespace InventorDxfExportAddin.Buttons
+{
+    public class DefaultButton : InventorButton
+    {
+        protected override void Execute(NameValueMap context, Inventor.Application inventor)
+        {
+            MessageBox.Show($"Current document name: {inventor.ActiveDocument.DisplayName}");
+        }
+
+        protected override string RibbonName => InventorRibbons.Drawing;
+
+        protected override string RibbonTabName => DrawingRibbonTabs.PlaceViews;
+
+        protected override string RibbonPanelName => "InventorDxfExportAddin";
+
+        protected override string Label => "DefaultButton";
+
+        protected override string Description => "Default Button Description";
+
+        protected override string Tooltip => "Click the Default Button";
+
+        protected override string LargeIconResourceName => "InventorDxfExportAddin.Buttons.Assets.Default-Light.png";
+
+        protected override string DarkThemeLargeIconResourceName => "InventorDxfExportAddin.Buttons.Assets.Default-Dark.png";
+
+        protected override string SmallIconResourceName => LargeIconResourceName;
+
+        protected override string DarkThemeSmallIconResourceName => DarkThemeLargeIconResourceName;
+    }
+}
