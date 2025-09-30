64653b914aa2976f5caf5654dd710036e57a926d
Kurt Jacobson <kjacobson@metalforming-usa.net>
2025-09-30 15:03:18 -0400

    Add buttons

--- Buttons/DefaultButton.cs
+++ Buttons/DefaultButton.cs
@@ -5,7 +5,35 @@ using static System.Windows.Forms.DataFormats;
 
 namespace InventorDxfExportAddin.Buttons
 {
-    public class DefaultButton : InventorButton
+    public class ExportDxfButton : InventorButton
+    {
+        protected override void Execute(NameValueMap context, Inventor.Application inventor)
+        {
+            MessageBox.Show($"Current document name: {inventor.ActiveDocument.DisplayName}");
+        }
+
+        protected override string RibbonName => InventorRibbons.Part;
+
+        protected override string RibbonTabName => PartRibbonTabs.SheetMetal;
+
+        protected override string RibbonPanelName => "Schröder DXF Export";
+
+        protected override string Label => "Export\nDXF";
+
+        protected override string Description => "Default Button Description";
+
+        protected override string Tooltip => "Click the Default Button";
+
+        protected override string LargeIconResourceName => "InventorDxfExportAddin.Buttons.Assets.Export-Light.png";
+
+        protected override string DarkThemeLargeIconResourceName => "InventorDxfExportAddin.Buttons.Assets.Export-Light.png";
+
+        protected override string SmallIconResourceName => LargeIconResourceName;
+
+        protected override string DarkThemeSmallIconResourceName => DarkThemeLargeIconResourceName;
+    }
+
+    public class DxfSettingsButton : InventorButton
     {
         protected override void Execute(NameValueMap context, Inventor.Application inventor)
         {
@@ -19,20 +47,54 @@ namespace InventorDxfExportAddin.Buttons
 
         protected override string RibbonTabName => PartRibbonTabs.SheetMetal;
 
-        protected override string RibbonPanelName => "Schroeder DXF Export";
+        protected override string RibbonPanelName => "Schröder DXF Export";
 
-        protected override string Label => "Export\nDXF";
+        protected override string Label => "DXF Settings";
+
+        protected override bool UseLargeIcon => false;
 
         protected override string Description => "Default Button Description";
 
         protected override string Tooltip => "Click the Default Button";
 
-        protected override string LargeIconResourceName => "InventorDxfExportAddin.Buttons.Assets.Default-Light.png";
+        protected override string LargeIconResourceName => "InventorDxfExportAddin.Buttons.Assets.Settings-Dark.png";
 
-        protected override string DarkThemeLargeIconResourceName => "InventorDxfExportAddin.Buttons.Assets.Default-Dark.png";
+        protected override string DarkThemeLargeIconResourceName => "InventorDxfExportAddin.Buttons.Assets.Settings-Dark.png";
 
         protected override string SmallIconResourceName => LargeIconResourceName;
 
         protected override string DarkThemeSmallIconResourceName => DarkThemeLargeIconResourceName;
     }
+
+    public class ExportOptionsButton : InventorButton
+    {
+        protected override void Execute(NameValueMap context, Inventor.Application inventor)
+        {
+            MessageBox.Show($"Current document name: {inventor.ActiveDocument.DisplayName}");
+        }
+
+        protected override string RibbonName => InventorRibbons.Part;
+
+        protected override string RibbonTabName => PartRibbonTabs.SheetMetal;
+
+        protected override string RibbonPanelName => "Schröder DXF Export";
+
+        protected override string Label => "Export Options";
+
+        protected override bool UseLargeIcon => false;
+
+
+        protected override string Description => "Default Button Description";
+
+        protected override string Tooltip => "Click the Default Button";
+
+        protected override string LargeIconResourceName => "InventorDxfExportAddin.Buttons.Assets.Settings-Dark.png";
+
+        protected override string DarkThemeLargeIconResourceName => "InventorDxfExportAddin.Buttons.Assets.Settings-Dark.png";
+
+        protected override string SmallIconResourceName => LargeIconResourceName;
+
+        protected override string DarkThemeSmallIconResourceName => DarkThemeLargeIconResourceName;
+    }
+
 }
